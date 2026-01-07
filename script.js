4a9b07881301c5f9815a7f59e3fb9eb3c3846505
Novak-creator <novsta080324@student.jenseneducation.se>
2026-01-07 01:36:44 +0100

    3.0 almost finished

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById("header").innerHTML = `
+    <nav>
+      <div class="nav-grupp">
+        <div class="logo">KodKraft</div>
+        <ul class="nav-links">
+          <li><a href="index.html">Startsida</a></li>
+          <li><a href="kurser.html">Kurser</a></li>
+          <li><a href="omoss.html">Om oss</a></li>
+          <li><a href="priser.html">Priser</a></li>
+        </ul>
+      </div>
+      <div class="btns"></div>
+      <button class="btn">Logga in</button>
+
+
+
+
+    </nav>
+  `;
+
+
+document.getElementById("footer").innerHTML = `
+<div class="footer-content">
+      <p>&copy; 2026 KodKraft. All rights reserved.</p>
+      <div class="footer-links">
+        <a href="#">Sekretesspolicy</a>
+        <a href="#">Användarvilkor</a>
+        <a href="#">Kontaka Oss</a>
+      </div>
+    </div>
+    `;
